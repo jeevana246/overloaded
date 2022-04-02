e2517188ee074d70e2f8ae4882c2ade9b82a96ba
jeevana246 <69810945+jeevana246@users.noreply.github.com>
2022-04-02 10:16:19 +0530

    Add files via upload

---
+++ record.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SIGN UP</title>
+	<link rel="stylesheet" type="text/css" href="record.css">
+</head>
+<body>
+<div class="container">
+        <div class="header">
+        <form action="connect.php" method="post" class="form" id="form">
+            <div class="float-container">
+
+                <div class="float-child">
+                    <div class="green">Float Column 1</div>
+                </div>
+                
+                <div class="float-child">
+                    <div class="blue">Float Column 2</div>
+                </div>
+            
+            </div>
+
+        </form>
+        </div>
+</div>
